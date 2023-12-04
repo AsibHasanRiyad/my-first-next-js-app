de2f4d8b8721d8a533e99e14e94e6ed3b9efcc77
AsibHasanRiyad <asibhasanriyad@gmail.com>
2023-12-04 16:40:45 +0600

    loading and error page

---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div className=' flex justify-center items-center min-h-screen'>
+            <h1 className='  text-3xl'>Error 404 !!! Page not fuound</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
